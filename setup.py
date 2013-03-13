09ca41ad9b2eb4719f0d91d5db9fb61221bdb571
Scott Griffin <scott@loggly.com>
2013-03-13 16:05:01 -0700

    Included for packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pymarketo',
+    author='WebReply',
+    author_email='',
+    maintainer='Scott Griffin',
+    maintainer_email='scott@loggly.net',
+    version='0.1',
+    packages=['marketo', 'tests'],
+    long_description=open( 'README.md' ).read(),
+    install_requires=open( 'requirements.txt' ).read().split()
+    )
