417db2cdce9ac93d9254fd9ab425e86d6ef54222
Scott Griffin <scott@loggly.com>
2013-03-13 16:05:44 -0700

    Changed readme in package

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     maintainer_email='scott@loggly.net',
     version='0.1',
     packages=['marketo', 'tests'],
-    long_description=open( 'README.md' ).read(),
+    long_description=open( 'README.rst' ).read(),
     install_requires=open( 'requirements.txt' ).read().split()
     )
